4a54def742afcba9540e5471b57ba08d5d41e652
habibbs <syawalihbb@gmail.com>
2023-06-07 23:01:46 +0700

    add autentikasi-token middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const User = require('../models').user;
+
+// Verifikasi token atau autentikasi token user
+module.exports.authenticate = (req, res, next) => {
+    const token = req.headers.authorization;
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+  
+    jwt.verify(token, process.env.jwtSecret, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+  
+      User.findByPk(decoded.id)
+        .then((user) => {
+          req.user = user;
+          next();
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json({ message: 'Internal server error' });
+        });
+    });
+  };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/login', authController.login);
+router.get('/users', authMiddleware.authenticate, (req, res) => {
+    res.send('Khusus user pemilik akun')
+});
+
 
 module.exports = router;
