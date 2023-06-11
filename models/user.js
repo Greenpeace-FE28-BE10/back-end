c3d51a147e4e8bc3aedefe1e7ee62a8780644ec7
Monika Angelia Panjaitan <monikamedan0102@gmail.com>
2023-06-12 01:36:59 +0700

    feat: add the getCommunityMembers & deleteCommunityMember function, also added some adjusment

--- models/user.js
+++ models/user.js
@@ -8,7 +8,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // Define association with CommunityUserModel
+      user.hasMany(models.communityUser, {
+        foreignKey: "users_id",
+        as: "CommunityUsers",
+      });
     }
   }
   user.init(
