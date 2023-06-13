cd6ba7cf2d319706a84e9dcbb1948c0182748ffe
Monika Angelia Panjaitan <monikamedan0102@gmail.com>
2023-06-13 20:32:42 +0700

    fix: fix the cors for registration endpoint

--- routes/guestRoutes.js
+++ routes/guestRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const CommunityController = require("../controllers/CommunityController.js");
 
 router.post("/login", authController.login);
-router.post("/register", authController.register);
+router.post("/register", cors(), authController.register);
 router.get("/communities", CommunityController.getAllCommunity);
 router.get("/communities/:id", CommunityController.getCommunityDetail);
 
